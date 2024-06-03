b29248cc9733206f0591e7d935e0a1f69dbdecdf
Manuel Buchfink <65283214+manuelbuchfink@users.noreply.github.com>
2024-06-03 16:41:08 +0200

    Delete openNPZ.py

--- openNPZ.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-data = np.load('/home/manuel/Documents/Bachelor Thesis/NeRP-main/data/ct_data/out_file.npz')
-lst = data.files
-np.set_printoptions(threshold=np.inf)
-for item in lst:
-    print(item.shape())
-   # print(data[item])
-#print(data[lst[0]][0][0])
