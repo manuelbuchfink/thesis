85e84ec9b94bdc8c1f8e3c5996ad5b04686c0d96
manuelbuchfink <st173195@stud.uni-stuttgart.de>
2024-11-27 08:44:26 +0100

    reorganize

---
+++ test_3d_small.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=24G
+#SBATCH --gpus=rtx4090:1
+#SBATCH -o ./Reports/slurm-%j.out # STDOUT
+
+source ~/.bashrc
+export PATH=/miniconda3/bin:$PATH
+
+conda deactivate
+conda activate odl
+
+module purge
+module load python/3.11/pytorch/
+
+srun --unbuffered --gpus=rtx4090:1 --ntasks=1 --cpus-per-task=32 --mem=24G python ner/train_ct_recon_3d_small.py --config ner/configs/ct_recon_3d_small.yaml
+
